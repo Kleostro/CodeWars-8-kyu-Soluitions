e66916a0f3ee3f48d08140ffbb8f09324e3a3492
Max <133934232+Kleostro@users.noreply.github.com>
2023-08-26 22:13:36 +0300

    feat: convert a Boolean to a String.js

---
+++ Convert a Boolean to a String.js
@@ -0,0 +1,12 @@
+/*
+Convert a Boolean to a String
+
+ESCRIPTION:
+
+  Implement a function which convert the given boolean value into its string representation.
+  
+  Note: Only valid inputs will be given.
+
+SOLUTION: 
+*/
+const booleanToString = (b) => b ? 'true' : 'false'
