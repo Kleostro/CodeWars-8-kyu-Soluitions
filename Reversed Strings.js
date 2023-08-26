376ad6e566869754cb9228108487cef56c8324f2
Max <133934232+Kleostro@users.noreply.github.com>
2023-08-26 19:45:04 +0300

    feat: reversed Strings.js

---
+++ Reversed Strings.js
@@ -0,0 +1,14 @@
+/*
+Even or Odd
+
+ESCRIPTION:
+
+DESCRIPTION:
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+SOLUTION: 
+*/
+const solution = (str) => str.split('').reverse().join('');
